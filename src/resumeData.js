9f21fdec6d786abdc698fa8e1df128d07c2bb642
ukr-TA <utsukkharel21@gmail.com>
2025-11-15 12:34:37 +0545

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Utsuk Kharel",
+    "role": "Frontend Developer and AI Enthusiast",
+    "linkedinId":"https://www.linkedin.com/in/utsukkharel",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles, learn about new innovations on AI or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/utsukkharel/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ukr-TA",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently a second year student at The Sunway collage Kathmandu and pursuing my Bachelors from here. I am a self taught Web Developer, currently diving deeper into AI & Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nepal",
+    "Mail":": Utsukkharel21@gmail.com",
+    "website":"https://github.com/ukr-TA",
+    "education":[
+      {
+        "Collage name":"Sunway Collage Kathmandu",
+        "specialization":"C.S with AI",
+        // "MonthOfPassing":"present",
+        "YearOfPassing":"present",
+        "Achievements":"-"
+      },
+      {
+        "School Name":"Panchayet secondary School",
+        "specialization":"Science with Computer science",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2022",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    // "portfolio":[
+    //   {
+    //     "name":"project1",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    // ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
